7cdc09606e03a25ce84d6b0f23bdab76c020eaf1
jack <jackchou00@outlook.com>
2024-09-28 14:40:55 +0800

    Add .DS_Store to .gitignore and generate 8-bit and 10-bit TIFF images

--- gelb_effect.py
+++ gelb_effect.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.image import imsave
 from PIL import Image
 import time
 
@@ -21,16 +22,30 @@ b=np.linspace(0,255,256)
 print(np.array_equal(a,b)), output: True
 """
 
-bit_depth = 10
-a = np.linspace(0, 1, 2**bit_depth)
 
-patch_width = 5
-b = np.zeros(2**bit_depth * patch_width)
+def generate_img(bit_depth=10, Image_width=3072):
+    a = np.linspace(0, 2**bit_depth - 1, 2**bit_depth)
 
-for i in range(0, patch_width):
-    b[i::patch_width] = a
+    Image_width = 3072
+    patch_width = Image_width // (2**bit_depth)
+    b = np.zeros(2**bit_depth * patch_width)
 
-Image_height = 1440
-c = np.zeros([1440, 2**bit_depth * patch_width])
-for i in range(0, Image_height):
-    c[i, ...] = b
+    for i in range(0, patch_width):
+        b[i::patch_width] = a
+
+    Image_height = 1440
+    c = np.zeros([1440, 2**bit_depth * patch_width])
+    for i in range(0, Image_height):
+        c[i, ...] = b
+    c = c.astype(np.uint16)
+    c = c * 2 ** (16 - bit_depth)
+
+    image = Image.fromarray(c, mode="I;16")
+    return image
+
+
+image_8 = generate_img(8)
+image_10 = generate_img(10)
+
+image_8.save("8bit.tiff")
+image_10.save("10bit.tiff")
